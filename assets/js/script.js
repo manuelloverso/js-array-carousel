f71073756120a156a62363e05addca5d1595e7ed
manuelloverso <manuelloverso@gmail.com>
2024-02-23 15:04:20 +0100

    add images in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+// Add the images into an array
+const imgArray = ["01", "02", "03", "04", "05"];
+
+let activeImg = 0;
+
+// Selecting carousel container
+const carousel = document.querySelector(".carousel");
+
+console.log(carousel, imgArray, activeImg);
+
+// Scrolling to generate array imgages into the dom
+for (let i = 0; i < imgArray.length; i++) {
+  let singleImg = imgArray[i];
+  // console.log(singleImg);
+  carousel.insertAdjacentHTML(
+    "beforeend",
+    `<img src="./assets/img/${singleImg}.webp" alt="" />`
+  );
+}
